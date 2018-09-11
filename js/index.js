c3ab78c75cb58dccdad3c64a7d68460afdd2e7d1
Roman Kalgin <kalginroman@gmail.com>
2018-09-11 19:23:09 +0000

    fix all responsive

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
-var myIndex = 0;
+let myIndex = 0;
 carousel();
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("myslides");
+    let i;
+    const x = document.getElementsByClassName("myslides");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";  
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(carousel, 2500); // Change image every 2 seconds
 }
 
-const linkWhatIsIt = document.querySelector('.group_1 a');
-linkWhatIsIt.href = 'https://trail-running-runingboy.c9users.io/what_it_is.html';
