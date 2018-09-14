def6ee7e2c429505451637c8b11a30f0b411f3b6
Roman Kalgin <kalginroman@gmail.com>
2018-09-14 13:13:20 +0000

    work with bootstrap

--- js/index.js
+++ js/index.js
@@ -1,23 +1,15 @@
-let myIndex = 0;
-carousel();
-
-function carousel() {
-    let i;
-    const slider = document.getElementsByClassName("myslides");
-    for (i = 0; i < slider.length; i++) {
-       slider[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > slider.length) {myIndex = 1}    
-    slider[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2500);
-}
-
-document.querySelector('.counting-registrations')
-    .addEventListener('click', countingRegistrations);
-    let startNumber = 1;
-function countingRegistrations(){
-    const display = document.querySelector('.display');
-    display.value = startNumber++;
-}
+//let myIndex = 0;
+//carousel();
+//
+//function carousel() {
+//    let i;
+//    const slider = document.getElementsByClassName("myslides");
+//    for (i = 0; i < slider.length; i++) {
+//       slider[i].style.display = "none";  
+//    }
+//    myIndex++;
+//    if (myIndex > slider.length) {myIndex = 1}    
+//    slider[myIndex-1].style.display = "block";  
+//    setTimeout(carousel, 2500);
+//}
 
