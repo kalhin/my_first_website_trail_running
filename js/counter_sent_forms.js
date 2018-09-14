95dac851bf0a605e1e722e6058e7c1d396de7ceb
Roman Kalgin <kalginroman@gmail.com>
2018-09-14 22:06:51 +0000

    fix send-counter

--- js/counter_sent_forms.js
+++ js/counter_sent_forms.js
@@ -1,7 +1,13 @@
-document.querySelector('.counting-registrations')
-    .addEventListener('click', countingRegistrations);
-    let i = 1;
-function countingRegistrations(){
-    const display = document.querySelector('.display');
-    display.value = i++;
-}
+document.querySelector('form')
+    .addEventListener('submit', function(e){
+    if(e.target.checkValidity()){
+        if (localStorage['send-counter'] === undefined){
+            localStorage['send-counter'] = 1;
+        } else {
+        localStorage['send-counter'] = parseInt(localStorage['send-counter']) + 1;
+        }
+    }
+});
+
+const display = document.querySelector('.display');
+display.value = localStorage['send-counter'];
