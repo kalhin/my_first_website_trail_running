be85597c1cd91a7b1293dfb26cd903759acd39fc
Roman Kalgin <kalginroman@gmail.com>
2018-09-14 15:27:53 +0000

    fix mistakes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-//let myIndex = 0;
-//carousel();
-//
-//function carousel() {
-//    let i;
-//    const slider = document.getElementsByClassName("myslides");
-//    for (i = 0; i < slider.length; i++) {
-//       slider[i].style.display = "none";  
-//    }
-//    myIndex++;
-//    if (myIndex > slider.length) {myIndex = 1}    
-//    slider[myIndex-1].style.display = "block";  
-//    setTimeout(carousel, 2500);
-//}
+let myIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    const slider = document.getElementsByClassName("myslides");
+    for (i = 0; i < slider.length; i++) {
+       slider[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > slider.length) {myIndex = 1}    
+    slider[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2500);
+}
 
