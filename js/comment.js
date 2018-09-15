b7045c77321b897916074f7439dcd392548cbea7
Roman Kalgin <kalginroman@gmail.com>
2018-09-15 15:53:42 +0000

    add pages TRAINING and EQUIPMANT

--- js/comment.js
+++ js/comment.js
@@ -1,12 +1,4 @@
 const addButtonComments = document.querySelector('.add_comments_button');
-addButtonComments.addEventListener('click', addName) 
-function addName() {
-        const textName = document.querySelector('.text_name');
-        const li = document.createElement('li');
-        li.innerText = textName.value;
-        document.querySelector('.name').appendChild(li);
-        textName.value = '';
-}
 addButtonComments.addEventListener('click', addComment)
 function addComment() {
         const textComment = document.querySelector('.text_comment');
